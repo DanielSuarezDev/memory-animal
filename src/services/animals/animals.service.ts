4a8a8068516d27a1db4e100040866b37d2c7abbf
Daniel Suarez Dev <danielsuarez3114@gmail.com>
2023-04-20 20:09:08 -0500

    config endpoint

--- src/services/animals/animals.service.ts
+++ src/services/animals/animals.service.ts
@@ -2,7 +2,7 @@ import { get } from "../../utils/AxiosAdapter";
 
 export const listAnimalsService = async (pairsCount: number): Promise<[]> => {
   // const uri = `https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20`;
-  const uri = `http://localhost:4000/api/animals?pairs_count=${pairsCount || 6}`;
+  const uri = `https://memory-animal-api.vercel.app/api/animals?pairs_count=${pairsCount || 6}`;
   const response = await get({
     url: uri,
   });
